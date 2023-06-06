770ae406ac9434ca61c127c434e7cd356ce57a72
NickMoo5 <nicholasamah@gmail.com>
2023-06-06 00:55:16 -0700

    init push

---
+++ src/test/java/cmpt276/studentdb/studentdb/StudentdbApplicationTests.java
@@ -0,0 +1,13 @@
+package cmpt276.studentdb.studentdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
